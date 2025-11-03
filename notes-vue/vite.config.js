f0b33563ac761287b3e5ea231292abf74769077e
Ronnie <2290196596@qq.com>
2025-11-03 16:03:40 +0800

    生成静态文件

--- notes-vue/vite.config.js
+++ notes-vue/vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const repoName = 'heart-period-notes'
+
 export default defineConfig({
     plugins: [vue()],
     server: {
         port: 3000
+    },
+    // Vite特有的配置
+    base: process.env.NODE_ENV === 'production'
+        ? `/${repoName}/`  // Gitee Pages子路径
+        : '/',             // 开发环境
+
+    build: {
+        outDir: '../dist',    // 构建输出目录
+        assetsDir: 'assets' // 静态资源目录
     }
 })
